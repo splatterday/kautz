bff6cae88f8e0197dd83c9e15411372371879147
splatterday <contactjohnlee@gmail.com>
2019-12-22 21:49:53 -0500

    Stepper brought into Display

--- src/Display.jsx
+++ src/Display.jsx
@@ -3,6 +3,47 @@ import Home from './pages/Home';
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
 
+class NavPt extends React.Component {
+    handleClick = () => {
+        const { path, onClick } = this.props;
+        onClick(path);
+        this.setState({currentPage: this.props.currentPage}) 
+    }
+    render() {
+        // if(this.state.active) {
+        //     {active}
+        // }
+        return(
+            <div className="navpt">
+                <a href={this.props.path} onClick={this.handleClick}>X</a>
+            </div>
+        );
+    }
+}
+
+class Stepper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: "home",
+        }
+    }
+
+    changePage(props) {
+        this.setState({currentPage: this.props.currentPage})
+    }
+
+    render() {
+        return(
+            <div className="stepper">
+                <NavPt currentPage="page1" onClick={this.changePage}/>
+                <NavPt currentPage="page2" onClick={this.changePage}/>
+                <NavPt currentPage="home" onClick={this.changePage}/>
+            </div>
+        )
+    }
+}
+
 class Display extends React.Component {
    
     render() {
@@ -20,6 +61,7 @@ class Display extends React.Component {
         return(
             <div className="app-display">
                 {display}
+                <Stepper/>
             </div>
         )
     }
